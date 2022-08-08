74844a7a737ad3eedf1288f5bd59ae0dfde0763e
Rajashree Behera <rajashree.behera@se.com>
2022-08-08 13:08:12 +0530

    cross origin commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "userslambda": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "usersdev": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { API } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cross-origin-sample2';
+  public userList: any;
+
+  async ngOnInit() {
+    let userInit = {
+      headers: {}
+    }
+    await API.get("usersdev", "/users", userInit).then(response => {
+      this.userList = response;
+      console.log('Response :::: ', this.userList);
+
+    }).catch(error => {
+      console.error(error);
+
+    })
+  }
 }
